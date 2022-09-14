0f8a14235adf4db053d58acb5429f59f521f145d
David Szmak <szmak.david@gmail.com>
2022-09-14 14:04:16 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# cardsCrud
-Create cards. Behind each card are two slightly rotated cards giving the illusion of a deck of cards. Functionality add new card with button that copies first card and add last in the list. Remove button removes the last card.
+# Cards Stacks
+
+## Install
+
+npm install
+
+## Start project
+
+gulp
---
+++ app/js/data.js
@@ -0,0 +1,57 @@
+export const data = [
+   { id: 1, name: "Cosmos", age: "37", image: 
+     "https://images.unsplash.com/photo-1604934797110-7c506bea3b3a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1445&q=80"
+   },
+   { id: 2, name: "Atlas", age: "39", image:
+     "https://images.unsplash.com/photo-1608643072426-a34159d0a762?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1474&q=80"
+   },
+   { id: 3, name: "Atlas", age: "39", image:
+     "https://images.unsplash.com/photo-1608643072426-a34159d0a762?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1474&q=80"
+   },
+   { id: 4, name: "Atlas", age: "39", image:
+     "https://images.unsplash.com/photo-1608643072426-a34159d0a762?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1474&q=80"
+   },
+   { id: 5, name: "Atlas", age: "39", image:
+     "https://images.unsplash.com/photo-1608643072426-a34159d0a762?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1474&q=80"
+   },
+   { id: 6, name: "Atlas", age: "39", image:
+     "https://images.unsplash.com/photo-1608643072426-a34159d0a762?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1474&q=80"
+   },
+   { id: 7, name: "Atlas", age: "39", image:
+     "https://images.unsplash.com/photo-1608643072426-a34159d0a762?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1474&q=80"
+   },
+   { id: 8, name: "Atlas", age: "39", image:
+     "https://images.unsplash.com/photo-1608643072426-a34159d0a762?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1474&q=80"
+   },
+   { id: 9, name: "Atlas", age: "39", image:
+     "https://images.unsplash.com/photo-1608643072426-a34159d0a762?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1474&q=80"
+   },
+   { id: 10, name: "Atlas", age: "39", image:
+     "https://images.unsplash.com/photo-1608643072426-a34159d0a762?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1474&q=80"
+   },
+   { id: 11, name: "denis", age: "37", image: 
+     "https://images.unsplash.com/photo-1608643072426-a34159d0a762?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1474&q=80"
+   },
+   { id: 12, name: "Atlas", age: "39", image:
+     "https://images.unsplash.com/photo-1608643072426-a34159d0a762?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1474&q=80"
+   },
+   { id: 13, name: "Atlas", age: "39", image:
+     "https://images.unsplash.com/photo-1608643072426-a34159d0a762?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1474&q=80"
+   },
+   { id: 14, name: "Atlas", age: "39", image:
+     "https://images.unsplash.com/photo-1608643072426-a34159d0a762?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1474&q=80"
+   },
+   { id: 15, name: "Atlas", age: "39", image:
+     "https://images.unsplash.com/photo-1608643072426-a34159d0a762?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1474&q=80"
+   },
+   { id: 16, name: "Atlas", age: "39", image:
+     "https://images.unsplash.com/photo-1608643072426-a34159d0a762?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1474&q=80"
+   },
+   { id: 17, name: "Atlas", age: "39", image:
+     "https://images.unsplash.com/photo-1608643072426-a34159d0a762?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1474&q=80"
+   },
+   { id: 18, name: "Atlas", age: "39", image:
+     "https://images.unsplash.com/photo-1608643072426-a34159d0a762?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1474&q=80"
+   }
+ ];
+
